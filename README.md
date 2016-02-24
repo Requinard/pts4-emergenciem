47250ecbe89e1b30ecb51d0e8a2015fb846445fd
requinard <david.diks@student.fontys.nl>
2016-02-24 14:04:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# EmergenCIEM
+
+## How to
+
+### Run Swagger Editor
+
+- Install NodeJS
+- Install NodeJS http-server `npm install -g http-server`
+- run `http-server /src/swagger-editor`
+- navigate to `localhost:8080`
---
+++ src/java-desktop/src/main/java/Main.java
@@ -0,0 +1,14 @@
+package java;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by david on 2/24/16.
+ */
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
